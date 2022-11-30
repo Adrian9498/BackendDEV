2b476b0c1be9b6c8f8ebf2a25016779865c3d12a
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2022-11-29 21:22:45 -0600

    Endpoints de especies y zoo en forma

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -20,15 +20,88 @@ const getAnimales = (req,res) =>{
     });
 }
 
+const getEspecies = async (req,res) =>{
+    
+    try{
+        let response = await pool.query('SELECT * FROM especie');
+        
+        if(Object.keys(req.query).includes('nombre_vulgar')){
+            const {nombre_vulgar} = req.query;
+            response = await pool.query('SELECT * FROM especie WHERE nombre_vulgar = $1',[nombre_vulgar]);
+        }else if(Object.keys(req.query).includes('nombre_cientifico')){
+            const {nombre_cientifico} = req.query;
+            response = await pool.query('SELECT * FROM especie WHERE nombre_cientifico = $1',[nombre_cientifico]);
+        }else if(Object.keys(req.query).includes('peligro_ext')){
+            const {peligro_ext} = req.query;
+            response = await pool.query('SELECT * FROM especie WHERE peligro_ext = $1',[peligro_ext]);
+        }
+
+        if(response.rowCount == 0){
+            res.status(200).json({"Estatus":"No existe ningun registro"});
+            return;
+        }
+
+        res.status(200).json(response.rows);
+
+    }catch(error){
+        res.status(500).json({"error": error.message});
+    }
+}
+
 const getZoo = async (req,res) =>{
-    const response = await pool.query('SELECT * FROM zoo');
-    console.log(response);
-    res.status(200).json(response.rows);
+    
+    try{
+
+        
+        let response = await pool.query('SELECT * FROM zoo');
+        const tipo_busqueda = req.query.tipo_busqueda;
+        const {ciudad,pais,tamanio} = req.query;
+
+        let dict = {
+            "pais": await pool.query('SELECT * FROM zoo WHERE pais = $1',[pais]),
+            "ciudad": await pool.query('SELECT * FROM zoo WHERE ciudad = $1',[ciudad]),
+            "ciudadPais" : await pool.query('SELECT * FROM zoo WHERE ciudad = $1 AND pais = $2',[ciudad,pais]),
+            "ciudadPaisTamanio": await pool.query('SELECT * FROM zoo WHERE ciudad = $1 AND pais = $2 AND tamanio = $3',[ciudad,pais,tamanio]),
+        }
+
+        if(Object.keys(req.query).length){  
+            response = dict[tipo_busqueda];
+        }
+
+        
+        
+        /*if(Object.keys(req.query).includes('ciudad') && Object.keys(req.query).includes('pais') ){
+            const {ciudad,pais} = req.query;
+            response = await pool.query('SELECT * FROM zoo WHERE ciudad = $1 AND pais = $2',[ciudad,pais]);
+        }else if(Object.keys(req.query).includes('ciudad')){
+            const ciudad = req.query.ciudad;
+            console.log(Object.keys(req.query));
+            // SELECT * FROM zoo WHERE ciudad = $1
+            response = await pool.query('SELECT * FROM zoo WHERE ciudad = $1',[ciudad]);
+        }else if(Object.keys(req.query).includes('pais')){
+            const pais = req.query.pais;
+            response = await pool.query('SELECT * FROM zoo WHERE pais = $1',[pais]);
+        }*/
+        
+        
+        if(response.rowCount == 0){
+            res.status(200).json({"Estatus":"No existe ningun registro"});
+            return;
+        }
+        
+        res.status(200).json(response.rows);
+        
+        
+    }catch(error){
+        res.status(500).json({"error": error.message});
+    }
+    
 };
 
 
 module.exports = {
     holaDev,
     getAnimales,
-    getZoo
+    getZoo,
+    getEspecies
 }
